750d9c5168977ff806a5a43fdd39e674311c704a
Lukas Babula <lukas.babula@gmail.com>
2025-02-25 18:44:09 +0100

    Ignore type declaration files during testing

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,7 @@
-module.exports = { setupFiles: ['dotenv/config'] }
+module.exports = { 
+  setupFiles: ['dotenv/config'],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/", // Ignore compiled files
+    "\\.d\\.ts$" // Ignore TypeScript declaration files
+  ], }
