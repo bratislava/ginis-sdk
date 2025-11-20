0923eb6513a6be4023260142c53331c15ca11160
Lukas Babula <lukas.babula@gmail.com>
2025-11-20 12:59:47 +0100

    Add test for fitered document list functionality

--- src/api/ude/__tests__/Seznam-dokumentu.test.ts
+++ src/api/ude/__tests__/Seznam-dokumentu.test.ts
@@ -25,4 +25,24 @@ describe('UDE-Seznam-dokumentu', () => {
 
     expect(data['Seznam-dokumentu'].length).toBeGreaterThan(0)
   }, 20_000)
+
+  test('Archive filter request', async () => {
+    const data = await ginis.ude.seznamDokumentu({
+      Stav: 'sejmuto',
+      'Vyveseno-od': '2024-01-01',
+      'Vyveseno-od-horni-mez': '2024-03-01',
+    })
+    const filteredData = await ginis.ude.seznamDokumentuFilterArchiv({
+      Stav: 'sejmuto',
+      'Vyveseno-od': '2024-01-01',
+      'Vyveseno-od-horni-mez': '2024-03-01',
+    })
+
+    expect(filteredData['Seznam-dokumentu'].length).toBeGreaterThan(0)
+    expect(filteredData['Seznam-dokumentu'].length).toBeLessThan(data['Seznam-dokumentu'].length)
+
+    const originalRecordIds = new Set(data['Seznam-dokumentu'].map((doc) => doc['Id-zaznamu']))
+    const filteredRecordIds = filteredData['Seznam-dokumentu'].map((doc) => doc['Id-zaznamu'])
+    expect(filteredRecordIds.every((id) => originalRecordIds.has(id))).toBe(true)
+  }, 20_000)
 })
