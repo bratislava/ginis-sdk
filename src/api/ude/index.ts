3fe98b21868d7ce36aa7decd4866d871aa7ee57c
Marek Bodinger <marek.bodinger@gmail.com>
2026-03-12 14:49:45 +0100

    Add nacistSouborStream to barrel file

--- src/api/ude/index.ts
+++ src/api/ude/index.ts
@@ -1,11 +1,13 @@
 import { detailDokumentu } from './Detail-dokumentu'
 import { nacistSoubor } from './Nacist-soubor'
+import { nacistSouborStream } from './Nacist-soubor-stream'
 import { seznamDokumentu, seznamDokumentuFilterArchiv } from './Seznam-dokumentu'
 import { seznamKategorii } from './Seznam-kategorii'
 
 export default {
   detailDokumentu,
   nacistSoubor,
+  nacistSouborStream,
   seznamDokumentu,
   seznamDokumentuFilterArchiv,
   seznamKategorii,
